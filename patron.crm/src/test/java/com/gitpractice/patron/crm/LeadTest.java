eb409565d52cf5f69e0c615c43a09f28593145c4
harshaay <harshaay1495@gmail.com>
2021-03-30 10:14:47 +0530

    lead class

---
+++ patron.crm/src/test/java/com/gitpractice/patron/crm/LeadTest.java
@@ -0,0 +1,21 @@
+package com.gitpractice.patron.crm;
+
+import org.testng.annotations.Test;
+
+public class LeadTest {
+@Test
+public void createleadTest()
+{
+	System.out.println("Create lead");
+}
+@Test
+public void modifyleadTest()
+{
+	System.out.println("modify lead");
+}
+@Test
+public void deleteleadTest()
+{
+	System.out.println("delete lead");
+}
+}
